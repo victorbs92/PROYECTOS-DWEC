fc34698f51dccc0e78d6436327bd1240ad36f4c6
victorbs <victorbs@alumnos.iesgalileo.es>
2020-11-05 12:01:33 +0100

    .

--- EJEMPLOS_CLASE/Tema 05/AlmacenamientoLocal/formulario.js
+++ EJEMPLOS_CLASE/Tema 05/AlmacenamientoLocal/formulario.js
@@ -41,3 +41,10 @@ if (localStorage.getItem("usuarios") === null) {
   localStorage.setItem("usuarios", JSON.stringify(["Juan", "Pedro", "Luis"]));
 } else {
 }*/
+
+localStorage.setItem("usuariox", JSON.stringify({
+  direccion: {
+    calle: "XXXXXXX",
+    localidad: "YYYYYY"
+  }
+}));
