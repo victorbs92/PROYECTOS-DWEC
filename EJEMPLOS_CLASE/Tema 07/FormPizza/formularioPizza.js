9b81afd92eb408979192b266895d249432a3ffe8
Victor <victorbs@alumnos.iesgalileo.es>
2020-11-12 11:50:37 +0100

    .

---
+++ EJEMPLOS_CLASE/Tema 07/FormPizza/formularioPizza.js
@@ -0,0 +1,27 @@
+//importar si es un modulo
+
+
+
+//declaracion de funciones
+
+
+
+//declaracion de clases
+
+
+
+//declaracion de variables
+const formulario = {
+
+}
+
+
+//tratamientos globales
+
+
+
+//eventos
+
+
+
+//exportar si es un modulo
