f7d3b16fce84864af4962d465c4e9b4dc5d46976
Victor <victorbs@alumnos.iesgalileo.es>
2020-12-12 23:18:05 +0100

    .

--- EJERCICIOS/Tema 08/Ejercicio 02/ejercicio02.js
+++ EJERCICIOS/Tema 08/Ejercicio 02/ejercicio02.js
@@ -2,15 +2,29 @@ tabla = document.querySelector("table");
 
 tabla.addEventListener("click",  function (event) {
     
-        if(event.target.nodeName == "TD"){
+        if(event.target.nodeName === "TD"){
            
             resetearDiana(tabla);
 
             event.target.style.backgroundColor = "greenyellow";
+
+           /* let celdas = tabla.querySelectorAll("td");
+
+            let r = Array.from(cela)
+            //celdas.asArray();
+            console.log(celdas.isArray());
+
+            let x = event.target;
+            celdas.indexOf(x)
+           console.log();
+            //console.log(celdas.findIndex(event.target));*/
             
         }
     
 });
+
+let x = [1,3];
+console.log(x.isArray());
 
 
 function resetearDiana(tabla) {
