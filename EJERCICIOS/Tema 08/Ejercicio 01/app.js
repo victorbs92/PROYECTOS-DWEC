c565ede3425b1e676775c6204dc8c70b8c3939ae
Victor <victorbs@alumnos.iesgalileo.es>
2020-12-12 22:23:28 +0100

    .

--- EJERCICIOS/Tema 08/Ejercicio 01/app.js
+++ EJERCICIOS/Tema 08/Ejercicio 01/app.js
@@ -1,4 +1,4 @@
-
+/*
 loop(document.body);
 
 function loop(node) {
@@ -13,6 +13,54 @@ function loop(node) {
             
         }
 
+    }
+
+}
+*/
+
+
+
+/*console.log(document.getElementById("a").children);
+
+if (document.getElementById("a").children > 0) {
+    console.log("77777777777");
+}*/
+
+recorrerNodo(document.getElementById("a"));
+//recorrerNodo(document.querySelector("table"))
+//recorrerNodo(document.body)
+
+function recorrerNodo(nodo) {
+   // console.log("AAAAAA");
+
+    if (nodo.nodeType == 3) {
+        console.log(nodo.nodeValue);
+    }
+
+
+    if (nodo.nodeType == 1) {
+        console.log(nodo.nodeName);
+
+        if (nodo.hasAttributes()) {
+
+            for (const iterator of nodo.attributes) {
+                console.log(iterator.nodeName + " = " + iterator.nodeValue);
+
+            }
+
+        }
+
+    }
+
+    if (nodo.hasChildNodes() > 0) {
+        let as = nodo.childNodes
+        //console.log("DDDDDDD");
+        recorrerNodo(as[0]);
+
+    }
+
+    if (nodo.nextSibling != null) {
+        recorrerNodo(nodo.nextSibling)
     }
 
 }
