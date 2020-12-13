06bf8ee1f5c545df7974b8b17ab6c0bb9c906546
Victor <victorbs@alumnos.iesgalileo.es>
2020-12-13 23:08:45 +0100

    .

--- EJERCICIOS/Tema 08/Ejercicio 02/ejercicio02.js
+++ EJERCICIOS/Tema 08/Ejercicio 02/ejercicio02.js
@@ -1,38 +1,42 @@
 tabla = document.querySelector("table");
 
-tabla.addEventListener("click",  function (event) {
-    
-        if(event.target.nodeName === "TD"){
-           
-            resetearDiana(tabla);
+tabla.addEventListener("click", function (event) {
 
-            event.target.style.backgroundColor = "greenyellow";
+    if (event.target.nodeName === "TD") {
 
-           /* let celdas = tabla.querySelectorAll("td");
+        resetearDiana(tabla);
 
-            let r = Array.from(cela)
-            //celdas.asArray();
-            console.log(celdas.isArray());
+        event.target.style.backgroundColor = "greenyellow";
 
-            let x = event.target;
-            celdas.indexOf(x)
-           console.log();
-            //console.log(celdas.findIndex(event.target));*/
-            
-        }
-    
+        if (event.target.previousElementSibling !== null) {
+            event.target.previousElementSibling.style.backgroundColor = "red";
+        };
+
+        if (event.target.nextElementSibling !== null) {
+            event.target.nextElementSibling.style.backgroundColor = "red";
+        };
+
+        let padre = event.target.parentNode;
+        let seleccionado = [...padre.children].findIndex(celda => celda == event.target);
+
+        if (padre.previousElementSibling !== null) {
+            padre.previousElementSibling.children[seleccionado].style.backgroundColor = "red";
+        };
+
+        if (padre.nextElementSibling !== null) {
+            padre.nextElementSibling.children[seleccionado].style.backgroundColor = "red";
+        };
+
+    }
+
 });
 
-let x = [1,3];
-console.log(x.isArray());
-
-
 function resetearDiana(tabla) {
-    
+
     let celdas = tabla.querySelectorAll("td");
-   
+
     celdas.forEach(element => {
-        element.style.backgroundColor = "white"; 
+        element.style.backgroundColor = "white";
     });
-  
+
 }
