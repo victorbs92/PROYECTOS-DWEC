90fffe89595dfa9ed93f2d9e832785bca42ca91e
victorbs <victorbs@alumnos.iesgalileo.es>
2021-02-12 13:55:15 +0100

    .

--- PRUEBAS/appPython/app.py
+++ PRUEBAS/appPython/app.py
@@ -1,4 +1,6 @@
 #python -m flask run
+
+# IMPORTS
 import itertools
 import random
 import math
@@ -16,24 +18,30 @@ import random
 import numpy as np
 
 
+#VARIABLES
+hiloFuerzaBruta = None
+hiloMontecarlo = None
+
 app = Flask(__name__)
 
+
+#CABECERAS Y METODOS
 @app.route("/", methods=["GET", "POST"])
 def index():
 
     try:
         minutos = request.args.get("minutosEjecucion")
         if(minutos != '' and minutos != None):
-            iniciarPrograma()
+            iniciarPrograma(hiloFuerzaBruta, hiloMontecarlo)
             return render_template("index.html", template2='minutos' + minutos)
         else:
-
+            pararPrograma(hiloFuerzaBruta, hiloMontecarlo)
             return render_template("index.html", template2="VVVVVVVVVV")
     except:
         return render_template("index.html", template2="aa" )   
 
 
-def iniciarPrograma():
+def iniciarPrograma(hiloFuerzaBruta, hiloMontecarlo):
     #abrimos el csv y cargamos la matriz con los datos del csv
     file = open(os.path.join(sys.path[0], "sampleSinCeros15.csv"), "r")
     matriz = np.loadtxt(file, delimiter=",")
@@ -63,4 +71,34 @@ def iniciarPrograma():
     print("KKKKKKKKKKKKKKKKKKK")
     #sys.exit()
 
+def pararPrograma(hiloFuerzaBruta, hiloMontecarlo):
+    
+    # Obtiene hilo principal
 
+    hilo_ppal = threading.main_thread()
+
+    # Recorre hilos activos para controlar estado de su ejecución
+
+    for hilo in threading.enumerate():
+
+    # Si el hilo es hilo_ppal continua al siguiente hilo activo
+        hilo.exit()
+    '''
+        if hilo is hilo_ppal:
+            continue
+
+        print(hilo.getName(), 
+          hilo.ident, 
+          hilo.isDaemon(), 
+          threading.active_count())
+
+        if(hilo.getName() == "fuerzaBruta"):
+            print("GGGGGGGGGGG")
+            hilo.exit()
+
+    print("JJJJJJJJJJJJJ")
+    print(hilo.getName(), 
+          hilo.ident, 
+          hilo.isDaemon(), 
+          threading.active_count())
+          '''
